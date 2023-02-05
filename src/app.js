5e4ff260d4d91f6da1b5bc5ba45fb497623a0157
Another1a <123756300+Another1a@users.noreply.github.com>
2023-02-05 21:23:10 +0000

    Updated the time

--- src/app.js
+++ src/app.js
@@ -2,8 +2,31 @@ let apiKey = "40t3642569434232b59fd0dfaf2o59af";
 let query = "London";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.temperature.current);
   let city = document.querySelector("#city");
@@ -14,6 +37,8 @@ function displayWeather(response) {
   humidity.innerHTML = response.data.temperature.humidity;
   let wind = document.querySelector("#wind");
   wind.innerHTML = response.data.wind.speed;
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 axios.get(apiUrl).then(displayWeather);
